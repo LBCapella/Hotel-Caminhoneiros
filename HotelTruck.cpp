d87d77e2f57040a9da17222697b3fe40e1b778a8
LBCapella <luanb.capella@gmail.com>
2023-05-26 17:30:12 -0300

    Menu Apresentação

--- HotelTruck.cpp
+++ HotelTruck.cpp
@@ -1,1 +1,79 @@
-//Atividade final ihul
+#include <stdio.h>
+#include <stdlib.h>
+
+void menuApresentacao()
+{
+    printf("                **************************************************\n");
+    printf("                *                                                *\n");
+    printf("                *               HOTEL CALIFORNIA                 *\n");
+    printf("                *                    Para                        *\n");
+    printf("                *               Caminhoneiro(a)s                 *\n");
+    printf("                *                                                *\n");
+    printf("                **************************************************\n");
+}
+
+void caminhao() 
+{
+    printf("                       _____________________________________________________\n");
+    printf("                      |                                                     |\n");
+    printf("             _______  |                                                     |\n");
+    printf("            / _____ | |                       BRAHMA                        |\n");
+    printf("           / /(__) || |                                                     |\n");
+    printf("  ________/ / |OO| || |                                                     |\n");
+    printf(" |         |-------|| |                                                     |\n");
+    printf("(|         |     -.|| |_______________________                              |\n");
+    printf(" |  ____   \\       ||_________||____________  |             ____      ____  |\n");
+    printf("/| / __ \\   |______||     / __ \\   / __ \\   | |            / __ \\    / __ \\ |\\\n");
+    printf("\\|| /  \\ |_______________| /  \\ |_| /  \\ |__| |___________| /  \\ |__| /  \\ \\|/|\n");
+    printf("   | () |                 | () |   | () |                  | () |    | () |\n");
+    printf("    \\__/                   \\__/     \\__/                    \\__/      \\__/\n");
+}
+
+void limpaTela()
+{
+    system("@cls||clear");
+}
+
+void mensgBoasVindas()
+{
+    printf("========================================================================\n");
+    printf("                             BOAS VINDAS                                \n");
+    printf("Bem-vindo ao Hotel California Para Caminhoneiros! Desfrute de uma\n"
+    "estadia luxuosa e exclusiva, especialmente projetada para caminhoneiros\n"
+    "como voce. Nossa equipe esta empenhada em proporcionar uma experiencia\n"
+    "unica e memoravel durante sua estadia.\n\n");
+    system("pause");
+    limpaTela();
+
+    printf("========================================================================\n");
+    printf("-> Localizacao estrategica: Situado em uma localizacao conveniente,\n" 
+    "com facil acesso as principais rodovias e rotas de transporte.\n\n");
+
+    printf("-> Instalacoes de luxo: Quartos espacosos,\n"
+    "bem decorados e com amenidades de qualidade.\n\n");
+
+    printf("->Servicos exclusivos para caminhoneiros: Estacionamento seguro,\n"
+    "areas de descanso, lavanderia dedicada e opcoes de refeicoes especiais.\n\n");
+
+    printf("-> Equipe dedicada: Hospitalidade calorosa e servico excepcional\n"
+    "para garantir uma estadia agradavel e confortavel.\n");
+    printf("========================================================================\n\n\n");
+
+    printf("                               SEJA BEM VINDO(A)                          \n\n\n");
+    
+    printf("========================================================================\n");
+
+}
+
+
+int main()
+{
+    menuApresentacao();
+    caminhao();
+    printf("\n");
+    mensgBoasVindas();
+
+
+
+    return 0;
+}
